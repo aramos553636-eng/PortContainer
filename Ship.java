3dca3882ac9a5ac1c409f4fb44021697be8de754
aramos553636-eng <a.ramos.553636@umindanao.edu.ph>
2025-09-18 19:51:38 +0800

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -3,12 +3,12 @@ package alfert;
 class Ship {
     String name;
     String captain;
-
+// setter
     Ship(String name, String captain) {
         this.name = name;
         this.captain = captain;
     }
-
+//getter
     public String toString() {
         return name + " | Capt. " + captain;
     }
